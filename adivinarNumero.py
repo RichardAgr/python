e96a86675f914df65888d882a1e2c534221077ec
Ricardo <richardyord@gmail.com>
2023-07-19 10:24:06 +0200

    se modularizo todo el juego con sus propios metodos cada funcionalidad

--- adivinarNumero.py
+++ adivinarNumero.py
@@ -1,15 +1,8 @@
 import random as rd
 import sys
 
-numero = rd.randint(0,100)
 
-MIN = 0
-MAX = 99
-
-minimo = MIN
-maximo = MAX
-
-def pedirNumero(invitacion):
+def pedirNumero(invitacion,minimo,maximo):
     invitacion += " entre " + str(minimo) + " y " + str(maximo) + ": "
     
     while True:
@@ -19,21 +12,29 @@ def pedirNumero(invitacion):
         except:
             print("Solo introduce numeros del 0 -> 99!!!",file=sys.stderr)
         else:
-            if MIN <= entrada <= MAX:
+            if minimo <= entrada <= maximo:
                 break 
 
     return entrada
 
-
-while True:
-    intento = pedirNumero("Adivine el numero")
-    if intento<numero:
-        print("Demasiado pequenio")
-        minimo = intento+1
-    elif intento>numero:
-        print("Demasiado grande")
-        maximo = intento-1
-    else:
-        print("!Ha ganado¡")
-        break
+def evaluarJuego(numero):
+    minimo = 0
+    maximo = 99
+    while True:
+        intento = pedirNumero("Adivine el numero",minimo,maximo)
+        if intento<numero:
+            print("Demasiado pequenio")
+            minimo = intento+1
+        elif intento>numero:
+            print("Demasiado grande")
+            maximo = intento-1
+        else:
+            print("!Ha ganado¡")
+            break
     
+
+def jugar():
+    numero = rd.randint(0,100)
+    evaluarJuego(numero)
+
+jugar()
